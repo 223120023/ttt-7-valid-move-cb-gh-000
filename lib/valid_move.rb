f15dafb9fd98e62fd622255e4753bc762b31e082
jane martin <223120023@student.unaki.ac.id>
2018-03-19 03:11:12 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,9 +1,9 @@
 # code your #valid_move? method here
 def valid_move?(board,index)
-  if position_taken?(board,index)
-    return false
-  else
+  if !position_taken?(board,index)&& index.between?(1,9)
     return true
+  else
+    return false
   end
 
 end
